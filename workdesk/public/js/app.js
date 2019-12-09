fa6b6325d41a9a7362ef9c3d59c665d83c11a870
andersonbaixinho <54605764+aclaymar@users.noreply.github.com>
2019-12-09 19:40:35 -0300

    Adicionado a parte o Botão Login e Cadastro, e eliminado Barra de Rolagem

--- workdesk/public/js/app.js
+++ workdesk/public/js/app.js
@@ -49592,8 +49592,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/stevenyeh/workdesk/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/stevenyeh/workdesk/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\ANDERSON\Desktop\Projeto-integrador-laravel\workdesk\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\ANDERSON\Desktop\Projeto-integrador-laravel\workdesk\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
